6c31291161fd4c756794f2c8f9de22987b2c6f94
Leon Tan <leon.arian.tan@gmail.com>
2020-12-10 20:10:28 +0100

    Implement Splat Accessor

--- pitch/src/lib.rs
+++ pitch/src/lib.rs
@@ -1,50 +1,71 @@
-use core::marker::PhantomData;
-
 #[derive(Debug, Clone)]
-struct SplatAccessor<A: AsRef<[B]>, B> {
+struct SplatAccessor<'a, B> {
     factor: usize,
     index: usize,
-    inner: A,
-    _data: PhantomData<B>,
+    inner: &'a [B],
 }
 
-impl<A: AsRef<[B]>, B> SplatAccessor<A, B> {
-    fn new(inner: A) -> Self {
+impl<'a, B> SplatAccessor<'a, B> {
+    fn new<A: 'a + AsRef<[B]>>(inner: &'a A) -> Self {
         Self {
             factor: 1,
             index: 0,
-            inner,
-            _data: PhantomData,
+            inner: inner.as_ref(),
         }
     }
 
-    fn splat(self) -> (Self, Self) {
-        //(Self { ..self.clone() }, Self { ..self })
-        todo!()
+    fn splat(&self) -> (Self, Self) {
+        (
+            Self {
+                factor: self.factor * 2,
+                index: self.index,
+                inner: self.inner,
+            },
+            Self {
+                factor: self.factor * 2,
+                index: self.index + self.factor,
+                inner: self.inner,
+            },
+        )
+    }
+
+    fn len(&self) -> usize {
+        self.inner.len() / self.factor
     }
 }
 
-impl<A, B> core::ops::Index<usize> for SplatAccessor<A, B>
-where
-    A: AsRef<[B]>,
-{
+impl<B> core::ops::Index<usize> for SplatAccessor<'_, B> {
     type Output = B;
     fn index(&self, index: usize) -> &B {
         &self.inner.as_ref()[index * self.factor + self.index]
     }
 }
 
-impl<A, B> core::ops::IndexMut<usize> for SplatAccessor<A, B>
-where
-    A: AsMut<[B]> + AsRef<[B]>,
-{
-    fn index_mut(&mut self, index: usize) -> &mut B {
-        &mut self.inner.as_mut()[index * self.factor + self.index]
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn splat() {
+        let vec = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        let splat = SplatAccessor::new(&vec);
+
+        assert_eq!(splat[1], 1);
+        assert_eq!(splat[6], 6);
+
+        let (left, right) = splat.splat();
+        assert_eq!(left[0], 0);
+        assert_eq!(right[0], 1);
+        assert_eq!(left[2], 4);
+        assert_eq![right[3], 7];
+
+        let (left, right) = right.splat();
+        assert_eq!(left[0], 1);
+        assert_eq!(left[1], 5);
+        assert_eq!(right[0], 3);
+        assert_eq!(right[1], 7);
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
