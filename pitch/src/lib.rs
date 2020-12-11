ac9ceda0538a63d99142af33fdb11f2fd916cf84
Leon Tan <leon.arian.tan@gmail.com>
2020-12-11 20:50:44 +0100

    Implement PitchShifter

--- pitch/src/lib.rs
+++ pitch/src/lib.rs
@@ -1,12 +1,27 @@
 #![allow(dead_code)]
 
+// TODO: #1 Use Complex<T> instead of Complex32 in FFT
+// TODO: #2 Use Index<> instead AsRef<> constraint in SplatAccessor
+// TODO: #3 <-(#2) Use VecDequeue in Input and Output Buffer
+// TODO: #4 Fixed Size Frames using own Types
+// TODO: #5 Wavelet Type
+// TODO: #6 <- (#5) Factor out Frontend into Frequencer
+// TODO: #7 <- (#5) Factor Resynth into Wavelet
+// TODO: #8 <- (#6, #7, #4) Factor out input and output buffer
+// TODO: #9 <- (#5) Custom algs
+
 pub mod fft;
 pub(crate) mod splat;
 
 use core::f32::consts::PI;
 use num_complex::Complex32;
 
-use crate::fft::fft;
+use crate::fft::{fft, rfft};
+
+pub struct FrequencyBin {
+    amplitude: f32,
+    frequency: f32,
+}
 
 pub struct PitchShifter {
     sample_rate: usize,
@@ -19,7 +34,8 @@ pub struct PitchShifter {
     in_buf: Vec<i16>,
     out_buf: Vec<i16>,
     output_index: usize,
-    phase_buf: Vec<f32>,
+    in_phase_buf: Vec<f32>,
+    out_phase_buf: Vec<f32>,
 }
 
 impl PitchShifter {
@@ -27,6 +43,11 @@ impl PitchShifter {
         if !frame_size.is_power_of_two() {
             return Err(());
         }
+
+        if step_size >= frame_size {
+            return Err(());
+        }
+
         Ok(Self {
             sample_rate,
             frame_size,
@@ -38,7 +59,8 @@ impl PitchShifter {
             in_buf: vec![],
             out_buf: vec![],
             output_index: 0,
-            phase_buf: vec![0.0; frame_size],
+            in_phase_buf: vec![0.0; frame_size],
+            out_phase_buf: vec![0.0; frame_size],
         })
     }
 
@@ -61,7 +83,9 @@ impl PitchShifter {
                 // apply windowing
                 .enumerate()
                 .map(|(k, x)| {
-                    (-0.5 * f32::cos(2.0 * PI * k as f32 / self.frame_size as f32) + 0.5) * x
+                    let window =
+                        -0.5 * f32::cos(2.0 * PI * k as f32 / self.frame_size as f32) + 0.5;
+                    window * x
                 })
                 // map to complex numbers
                 .map(|x| Complex32::new(x, 0.0))
@@ -79,31 +103,107 @@ impl PitchShifter {
                 .enumerate()
                 .map(|(k, (amp, phase))| {
                     // get the phase difference to prior frame and update
-                    let phase_diff = phase - self.phase_buf[k];
-                    self.phase_buf[k] = phase_diff;
+                    let mut phase_diff = phase - self.in_phase_buf[k];
+                    self.in_phase_buf[k] = phase_diff;
 
-                    // calculate the new phase difference
+                    // calculate difference to expected phase
+                    phase_diff -=
+                        k as f32 * 2.0 * PI * (self.step_size as f32 / self.frame_size as f32);
 
-                    (amp, phase)
+                    // map back onto rad
+                    if phase_diff > PI {
+                        phase_diff -= PI;
+                    }
+                    if phase_diff < -PI {
+                        phase_diff += PI;
+                    }
+                    debug_assert!(phase_diff < PI && phase_diff > -PI);
+
+                    // compute frequency deviation
+                    let freq_dev = self.oversampling_rate * phase_diff / (2.0 * PI);
+
+                    // compute frequency
+                    let freq = (k as f32 + freq_dev) * self.freqs_per_bin;
+
+                    FrequencyBin {
+                        amplitude: amp,
+                        frequency: freq,
+                    }
                 })
                 .collect::<Vec<_>>();
 
-            todo!()
+            // manipulate the frequencies
+            let new_freqs = freqs
+                .iter()
+                .map(|bin| FrequencyBin {
+                    amplitude: bin.amplitude,
+                    frequency: bin.frequency * self.pitch_shift,
+                })
+                .collect::<Vec<_>>();
+
+            // do the reverse steps
+            let output = new_freqs
+                .iter()
+                // calculate back to amplitude and phase
+                .enumerate()
+                .map(|(k, bin)| {
+                    // calculate frequency deviation from frequency
+                    let freq_dev = bin.frequency / self.freqs_per_bin - k as f32;
+
+                    // calculate phase difference from frequency deviation
+                    let mut phase_diff = 2.0 * PI * freq_dev / self.oversampling_rate;
+
+                    // add possible overlap
+                    phase_diff +=
+                        k as f32 * 2.0 * PI * (self.step_size as f32 / self.frame_size as f32);
+
+                    // add phase diff to output phase buffer
+                    self.out_phase_buf[k] += phase_diff;
+
+                    // return amplitude and phase
+                    (bin.amplitude, self.out_phase_buf[k])
+                })
+                // turn back into complex numbers
+                .map(|(amp, phase)| Complex32::from_polar(amp, phase))
+                // remove the imaginary part
+                .map(|x| Complex32::new(x.re, 0.0))
+                .collect::<Vec<_>>();
+
+            // reverse fft
+            let output = rfft(&output).unwrap();
+
+            // apply window and turn into real numbers
+            let output = output
+                .iter()
+                .enumerate()
+                .map(|(k, x)| {
+                    let window =
+                        -0.5 * f32::cos(2.0 * PI * k as f32 / self.frame_size as f32) + 0.5;
+                    2.0 * window * x.im * (self.frame_size as f32 / self.oversampling_rate as f32)
+                })
+                // quantize
+                .map(|x| x as i16)
+                .collect::<Vec<_>>();
+
+            // add new output to buffer
+            self.out_buf.extend_from_slice(&output[..]);
+
+            // remove data from input
+            self.in_buf.drain(..audio.len());
         }
 
         // Copy audio from output buffer to the audio buffer
-        // Output index is a pointer into the out_buf, which creases every
-        // time we poll for output
-        if self.output_index + audio.len() >= self.out_buf.len() {
+        if audio.len() >= self.out_buf.len() {
             // If there is not enough data, we extend the buffer with 0 bytes
-            let bytes_left = self.out_buf.len() - self.output_index;
-            audio[..bytes_left].copy_from_slice(&self.out_buf[self.output_index..]);
+            let bytes_left = self.out_buf.len();
+            audio[..bytes_left].copy_from_slice(&self.out_buf[..]);
             audio[bytes_left..].iter_mut().for_each(|x| *x = 0);
-            self.output_index = self.out_buf.len();
+
+            // empty the buffer completely
+            self.out_buf.clear();
         } else {
-            let new_output_index = self.output_index + audio.len();
-            audio.copy_from_slice(&self.out_buf[self.output_index..new_output_index]);
-            self.output_index = new_output_index;
+            audio.copy_from_slice(&self.out_buf[..audio.len()]);
+            self.out_buf.drain(..audio.len());
         }
     }
 }
