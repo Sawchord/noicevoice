a4a87fd3ee34327977cb5f3bfa3643feea6f559a
Leon Tan <leon.arian.tan@gmail.com>
2020-12-15 19:57:05 +0100

    Add tooltips

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,4 @@
 #![recursion_limit = "256"]
-
 mod voice;
 use yew::prelude::*;
 
@@ -83,6 +82,7 @@ impl Component for Model {
                                 class=("button level-item column is-large", btn_color)
                                 style="display:block; width:100%; justify-content:center;"
                                 onclick = self.link.callback(|_|Msg::PlayButtonPress)
+                                data-tooltip="Start/Stop playback"
                             >
                             <span class="icon">
                                 <i class=("fas is-large", btn_icon)></i>
@@ -92,8 +92,13 @@ impl Component for Model {
                     </section>
 
                     <div class="card-content">
-                        {self.slider("volume", "Volume", "1", "0", "100")}
-                        {self.slider("pitch", "Pitch", "0.01", "0.5", "2.0")}
+                        {self.slider("volume", "Volume", "1", "0", "100",
+                            "Set the volume of the output. \
+                            Be careful to avoid loopbacks."
+                        )}
+                        {self.slider("pitch", "Pitch", "0.01", "0.5", "2.0",
+                            "Change the pitch of the output, without changing playback speed."
+                        )}
                     </div>
                 </div>
             </div>
@@ -102,9 +107,24 @@ impl Component for Model {
 }
 
 impl Model {
-    fn slider(&self, id: &str, name: &str, step: &str, min: &str, max: &str) -> Html {
+    fn slider(
+        &self,
+        id: &str,
+        name: &str,
+        step: &str,
+        min: &str,
+        max: &str,
+        tooltip: &str,
+    ) -> Html {
         html! {
             <div class="columns level">
+                <button
+                    class="button is-rounded is-light is-small has-tooltip-multiline"
+                    data-tooltip=tooltip
+                    style="width:36px; height:36px;"
+                >
+                    <i class="fas fa-info"></i>
+                </button>
                 <p
                     class="column level-item is-one-fifths"
                     style="min-width:50px; max-width:100px"
